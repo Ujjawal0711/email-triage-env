62cad71f58b153276a1e97e04858e34bbd71a7f4
Ujjawal Chaudhary <Ujjawal.ary@gmail.com>
2026-04-09 00:09:11 +0530

    fix: resolve module import error and port

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from openenv.core.env_server import create_app
-from email_triage_environment import EmailTriageEnvironment
+from server.email_triage_environment import EmailTriageEnvironment
 from models import EmailTriageAction, EmailTriageObservation
 
 env = EmailTriageEnvironment()
